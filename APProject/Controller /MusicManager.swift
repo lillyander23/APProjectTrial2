a1e7da33b9dd83184a4d838b56e1c8d4de05fa42
lillyander23 <lillyanderson676@gmail.com>
2023-05-12 20:59:34 -0500

    DONE

--- APProject/Controller /MusicManager.swift
+++ APProject/Controller /MusicManager.swift
@@ -50,9 +50,9 @@ class MusicManager: ObservableObject{
             self.songs = allSongs
         }
     }
-    
+
     func updateLike(song: SongStored){
-    
+
         // Figure out the index of the song we want to play in allSongs
         var index = 0
         for i in 0..<allSongs.count{
@@ -60,11 +60,11 @@ class MusicManager: ObservableObject{
                 index = i
             }
         }
-        
+
         allSongs[index].liked.toggle()
     }
 
-    
+
     
  func playSong(song: SongStored) { //https://stackoverflow.com/questions/59404039/how-to-play-audio-using-avaudioplayer-in-swiftui-project used this to create the play and pause functions also used this https://www.hackingwithswift.com/forums/swiftui/playing-sound/4921
     
